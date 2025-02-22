f61d60f9a90ca6870eb9558d903f3e74c0cee915
basedBaba <basedBaba@proton.me>
2025-02-22 21:28:08 +0530

    lol

--- lol.py
+++ lol.py
@@ -1,15 +1,25 @@
-def hello():
-    print("Hello, Python!")
+import os, sys, json  # Unnecessary imports
 
-hello()
+x = 10
+y=  20 # Bad spacing
+def badFunction (  a,b ):# No docstring, bad spacing
+  if a > 0:print("Positive") # No indentation, inline statement
+  else:
+      print ("Negative" )  # Extra spaces, inconsistent indentation
+  return x+y # Unused variables, bad return
 
-# Unused variable (flake8 will flag this)
-unused_var = 100
+def anotherFunc():
+        pass # Bad indentation
 
-# Indentation error (flake8 will flag this)
-if True:
-print("Indentation issue detected!")  
+class testClass: # Class name should be in PascalCase
+ def __init__( self,x,y ):
+      self.x=x
+      self.y = y
+ def method_1( self ): return self.x*self.y  # Inline return, bad spacing
 
-# Insecure use of eval (bandit will flag this)
-user_input = input("Enter something: ")
-eval(user_input)
+ def method2(self,    a,b): # Inconsistent method naming, bad spacing
+  if(a>b): return a   # No indentation
+  else:
+       return b
+
+def bad_
