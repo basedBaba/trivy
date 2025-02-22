63458fe4a1c98c5b4ba6ee9dd28f5fc39a5f922e
basedBaba <basedBaba@proton.me>
2025-02-22 21:29:59 +0530

    lol

--- lol.go
+++ lol.go
@@ -1,18 +1,7 @@
-package main
-
-import "fmt"
-
-func main() {
-    fmt.Println("Hello, Go!")
-    testFunc()
-}
-
-// Unused function (golangci-lint will flag this)
-func unusedFunction() {
-    fmt.Println("This function is not used")
-}
-
-// Function with linting issue (missing comment)
-func testFunc() {
-    fmt.Println("Testing Go linting")
-}
+def bad_function():  
+    a=1
+	b = 2  # Mixed spaces and tabs  
+c = "This is a very long line that will exceed the character limit set by Flake8 which is usually 79 or 88 characters"
+for i in range(10):print(i)  # Multiple statements on one line
+x,y,z=1,2,3  # Missing whitespace after comma
+foo = "unused variable"  
